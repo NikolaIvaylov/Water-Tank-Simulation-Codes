213fbe86dd7fecc8936d6e968cb36bd3157f7bf2
NikolaIvaylov <101429614+NikolaIvaylov@users.noreply.github.com>
2022-03-12 16:36:17 +0100

    Update (2nd order RK) Water Tank.py

--- (2nd order RK) Water Tank.py
+++ (2nd order RK) Water Tank.py
@@ -6,29 +6,30 @@ from matplotlib import pyplot as plt
 
 #Variables you can play with:
 #Time:
-t_step = 0.01 #s
-n = 3000 #number of steps
+t_step = 1 #s
+n = 300 #number of steps
 #Cylinder Tank:
-rc = 1 #radius (m)
+rc = 2 #radius (m)
 hc = 30 #height (m)
 Pf = 100 #What % of the tank is full?
 #Water:
 vin = 1 #speed of water flowing in (m/s)
 #Holes:
 rin = 0.1 #inlet radius (m)
-rout = 0.3 #outlet radius (m)
+rout = 0.2 #outlet radius (m)
 #RK constants:
-a2 = 2/3 #can also be 1/2
+a2 = 1/2 #can also be 1/2
 
 #Other variables (that you cannot play with):
 #Time:
 t = np.linspace(0, (n*t_step), n) #generate x-axis values
+t_step=t_step #correct t_step back to initial value
 #Cylinder
 Ab = np.pi*(rc**2) #base area (m^2)
 #Water Variables:
 #Water:
 h0 = hc*(Pf/100) #initial water level (m)
-vout = math.sqrt(2*6.67408*h0) #speed of water flowing out (m/s)
+vout = math.sqrt(2*9.81*h0) #speed of water flowing out (m/s)
 #Holes:
 Ain = np.pi*(rin**2) #inlet cross-sectional area (m^2)
 Aout = np.pi*(rout**2) #outlet cross-sectional area (m^2)
@@ -38,30 +39,24 @@ Qout = Aout*vout #rate of water flowing out (m^3/s)
 water_heights = [] #heights values for the y-axis
 water_heights.append(h0) #add initial water level
 #Runge-Kutta:
-a1 = 1-a2
-p1_q11 = 1/(2*a2)
-k1 = (Qin-Qout)/Ab #k1 = x(n)/Ab
-k2 = ((Qin-Qout)+(p1_q11*t_step))/Ab #k2 = (x(n)+p1*(time step)/Ab
-# ! the normal formula doesn't work for VERY large cross-sectional areas  !
-# ! this is why Mr. Williams specified a constant cross-sectional area    !
-# ! these changes to the formula DO NOT interfere with the normal results !
+a1 = 1-a2 #calculating a1
+p1_q11 = 1/(2*a2) #calculating p1 = q11 = 1/(2*a2)
 
-#2nd order Runge-Kutta Method:
 for time in range(1, len(t)):
     previous_height = water_heights[time-1] #get h(n-1)
-    vout = math.sqrt(2*6.67408*previous_height) #recalculate vout(h(n-1))
+    vout = math.sqrt(2*9.81*previous_height) #recalculate vout(h(n-1))
     Qout = Aout*vout #recalculate Qout with the new vout(h(n-1))
-    k1 = (Qin-Qout)/Ab #recalculate k1
-    k2 = ((Qin-Qout)/Ab)+((p1_q11*t_step)/Ab) #recalculate k2
-    current_water_height = previous_height + ((k1/2)+(k2/2))*t_step #h(n) = h(n-1) + (k1*(1/2)+k2*(1/2))*(time step)
+    k1 = (2*Qin)/Ab #calculating k1
+    k2 = -(2*p1_q11*Qout)/Ab #calculating k2
+    current_water_height = previous_height+(a1*k1+a2*k2)*t_step #h(n) = h(n-1) + (the change in height)*(time step)
     water_heights.append(current_water_height) #add the value of the current height to the y-axis
-        
+
 #Making sure that the numbers are within the cylinder's parameters:
     if water_heights[time] < 0:
         water_heights[time] = 0
     elif water_heights[time] > hc:
         water_heights[time] = hc
-        
+    
 #Displaying the graph:
 water_heights = np.array(water_heights)
 plt.figure(dpi=300) 
@@ -70,4 +65,5 @@ plt.xlabel("Time (s)")
 plt.ylabel("Height (m)")
 plt.plot(t, water_heights, color="blue")
 plt.plot(t, np.zeros(len(t)) , color="gray")
+plt.plot(t, [hc]*len(t) , color="gray")
 plt.show()
