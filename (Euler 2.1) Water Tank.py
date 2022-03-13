b0733c794b2bc86e35f811489e699faffbc38b69
NikolaIvaylov <101429614+NikolaIvaylov@users.noreply.github.com>
2022-03-13 14:06:41 +0100

    Update and rename (Euler) Water Tank.py to (Euler 2.1) Water Tank.py

--- (Euler 2.1) Water Tank.py
+++ (Euler 2.1) Water Tank.py
@@ -6,22 +6,22 @@ from matplotlib import pyplot as plt
 
 #Variables you can play with:
 #Time:
-t_step = 1 #s
-n = 300 #number of steps
+t_step = 0.5 #s
+n = 90 #number of steps
 #Cylinder Tank:
 rc = 2 #radius (m)
-hc = 30 #height (m)
+hc = 50 #height (m)
 Pf = 100 #What % of the tank is full?
 #Water:
 vin = 1 #speed of water flowing in (m/s)
 #Holes:
 rin = 0.1 #inlet radius (m)
-rout = 0.2 #outlet radius (m)
+rout = 0.1 #outlet radius (m)
 
 #Other variables (that you cannot play with):
 #Time:
 t = np.linspace(0, (n*t_step), round(n/2)) #generate x-axis values
-t_step=t_step*2 #correct t_step back to initial value
+t_step = t_step*2 #correct the time step to its initial value
 #Cylinder
 Ab = np.pi*(rc**2) #base area (m^2)
 #Water Variables:
@@ -36,26 +36,51 @@ Qout = Aout*vout #rate of water flowing out (m^3/s)
 #Other:
 water_heights = [] #heights values for the y-axis
 water_heights.append(h0) #add initial water level
-
-#Euler's Method:
+ 
+# Consider a differential equation: dh / dt = ((Qin - Qout) / Ab)t
+def dhdt(t, h):
+    #Make sure that the numbers are within the cylinder's parameters:
+    if h < 0:
+        h = 0
+    elif h > hc:
+        h = hc
+    vout = math.sqrt(2 * 9.81 * h) #recalculate vout(h(n-1))
+    Qout = Aout*vout #recalculate Qout with the new vout(h(n-1))
+    return ((Qin - Qout) / Ab) * t
+     
+# Function for Euler's Method:
+def euler(t0, h, t_step, tn):
+    while t0 < tn:
+        # Apply Euler's Formula to find next 
+        # value of h by using the dhdt() function:
+        h = h + t_step * dhdt(t0, h)
+        # Update next value of t:
+        t0 = t0 + t_step
+    #Make sure that the numbers are within the cylinder's parameters:
+    if h < 0:
+        h = 0
+    elif h > hc:
+        h = hc
+    #return the new water height: 
+    return h
+     
+#Euler's Method Loop:
 for time in range(1, len(t)):
     previous_height = water_heights[time-1] #get h(n-1)
-    vout = math.sqrt(2*9.81*previous_height) #recalculate vout(h(n-1))
-    Qout = Aout*vout #recalculate Qout with the new vout(h(n-1))
-    current_water_height = previous_height+((Qin-Qout)/Ab)*t_step #h(n) = h(n-1) + (the change in height)*(time step)
+    #Use the Euler's Method function:
+    current_water_height = euler(0, previous_height, t_step, t[time])
+    #Make sure that the numbers are within the cylinder's parameters:
+    if current_water_height < 0:
+        current_water_height = 0
+    elif current_water_height > hc:
+        current_water_height = hc
     water_heights.append(current_water_height) #add the value of the current height to the y-axis
-     
-#Making sure that the numbers are within the cylinder's parameters:
-    if water_heights[time] < 0:
-        water_heights[time] = 0
-    elif water_heights[time] > hc:
-        water_heights[time] = hc
 
 #Displaying the graph:        
 water_heights = np.array(water_heights)
 plt.figure(dpi=300) 
 plt.title(f"Euler's Method (step={t_step/2})")
-plt.xlabel("Time (s)")
+plt.xlabel("Time (m)")
 plt.ylabel("Height (m)")
 plt.plot(t, water_heights, color="blue")
 plt.plot(t, np.zeros(len(t)) , color="gray")
